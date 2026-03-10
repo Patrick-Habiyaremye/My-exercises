7be8f45d27cf4189d39bd76b9602b5382c6df883
patrick <habiyaremyepatrick.hp@gmaul.com>
2026-03-10 18:23:07 +0200

    ts-exercises1

---
+++ main.ts
@@ -0,0 +1,17 @@
+// Task Instructions
+// Create a playerName variable with the explicit type string and set it to Riven.
+// Create a playerLevel variable with the explicit type number and set it to 7.
+// Create a isPremiumMember variable with the explicit type boolean and set it to true.
+// Print the three variables using console.log(), one per line.
+// Print them in this exact order:
+// playerName
+// playerLevel
+// isPremiumMember
+
+let playerName: string = "Riven";
+let playerLevel: number = 7;
+let isPremiumMember: boolean = true;
+
+console.log(playerName);
+console.log(playerLevel);
+console.log(isPremiumMember);
